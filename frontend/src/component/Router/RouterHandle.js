fd8a9ba0f59a79ddeadb7dd7899f0c9005bdb133
= <=>
2022-01-12 13:11:07 +0600

    added react dom v6

---
+++ frontend/src/component/Router/RouterHandle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from '../Slider/Slider';
+
+const RouterHandle = () => {
+    return (
+        <div>
+            <Slider/>
+        </div>
+    );
+};
+
+export default RouterHandle;
