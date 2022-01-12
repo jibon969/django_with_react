d712e33d0c7c51daeb8876ca46e578de955cd059
= <=>
2022-01-12 23:53:57 +0600

    added Navbar component

--- frontend/src/component/About/About.js
+++ frontend/src/component/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from "../Navbar/Navbar";
 
 const About = () => {
 
     return (
         <div>
+            <Navbar/>
             <h3>I am About</h3>
         </div>
     )
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from "../Navbar/Navbar";
 import Slider from '../Slider/Slider';
 
 const Home = () =>{
     return(
         <div>
+            <Navbar/>
             <Slider/>
         </div>
     )
