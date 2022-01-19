70a9dea2f9359db32f80e9b454fea01f4bb99e74
= <=>
2022-01-19 23:09:46 +0600

    added footer component

---
+++ frontend/src/component/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () =>{
+    var style = {
+        color: 'white',
+        backgroundColor: 'black',
+        textAlign: 'center',
+        padding: "10px",
+    };
+    return(
+        <div>
+            <h2 style={style}>Hello i am  Footer</h2>
+        </div>
+    )
+};
+
+export default Footer;
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "../Navbar/Navbar";
 import Slider from '../Slider/Slider';
+import Footer from "../Footer/Footer";
 
 const Home = () =>{
     return(
         <div>
             <Navbar/>
             <Slider/>
+            <Footer/>
         </div>
     )
 };
