a5639281e716c98839bdafec39cf4a36d17d96f1
= <=>
2022-01-23 16:43:33 +0600

    added post single page dynamic

--- frontend/src/component/Post/Post.js
+++ frontend/src/component/Post/Post.js
@@ -1,27 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
-import image from '../../static/images/blog/post1.png'
 
-const Post = ()=>{
-
+const Post = ()=> {
     var style = {
         height: '200px',
         width: 'auto'
     };
 
-
     const [post, setPostData] = useState([]);
-
     useEffect(() => {
+
         fetch('http://127.0.0.1:8000/api/post-list/')
             .then(res => res.json())
             .then(data => setPostData(data))
     }, []);
-
-    // Data will show only console log
-    // fetch('http://127.0.0.1:8000/api/post-list/')
-    //     .then(response => response.json())
-    //     .then(json => console.log(json));
 
     return(
         <div>
@@ -35,8 +27,9 @@ const Post = ()=>{
                                 <img style={style} className="card-img-top" src={post.image} alt=""/>
                                 <div className="card-body">
                                     <h3 className="">{post.title}</h3>
+                                    <h3 className="">{post.id}</h3>
                                     <p>{post.description}</p>
-                                    <Link to="" className="btn btn-primary btn-block">More Detail</Link>
+                                    <Link to={`post-detail/${post.id}`} className="btn btn-primary btn-block">More Detail</Link>
                                 </div>
                             </div>
                         </div>
